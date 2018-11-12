2c75b509d491a1bd9fd5542b9bd505b5bda50ad3
lily_bell514 <lilybell.5555@gmail.com>
2018-11-12 19:05:37 +0900

    Make deal table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181110042525) do
+ActiveRecord::Schema.define(version: 20181112095350) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "first_name", null: false
@@ -34,8 +34,16 @@ ActiveRecord::Schema.define(version: 20181110042525) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "deals", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "image_url"
+    t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
